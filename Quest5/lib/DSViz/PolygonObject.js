0265abb289297e6b2c789ff6928420685e97533c
zr002 <117235379+zr002@users.noreply.github.com>
2025-03-16 15:09:07 -0400

    Update PolygonObject.js

--- Quest5/lib/DSViz/PolygonObject.js
+++ Quest5/lib/DSViz/PolygonObject.js
@@ -7,7 +7,7 @@
  */
 
 import SceneObject from "/lib/DSViz/SceneObject.js";
-import Polygon from "./lib/DS/Polygon.js";
+import Polygon from "/lib/DS/Polygon.js";
 
 export default class PolygonObject extends SceneObject {
   constructor(device, canvasFormat, filename) {
