35cc52d8d4e711ac45fa13f6c87250d5a17732fd
zr002 <117235379+zr002@users.noreply.github.com>
2025-03-23 18:00:03 -0400

    Update PointLight.js

--- Quest8/lib/Viz/PointLight.js
+++ Quest8/lib/Viz/PointLight.js
@@ -3,7 +3,7 @@
  * (License text omitted)
  */
 
-import Light from "/lib/Viz/Light.js"
+import Light from "/Quest8/lib/Viz/Light.js"
 
 export default class PointLight extends Light {
   constructor(intensity = [1, 1, 1], position = [0, 0, 0], attenuation = [1, 0.1, 0.01]) {
